a14b2ed444bce3eb711efbe2f4d7b4a60eead57d
Sohmteee <sohmtee@gmail.com>
2022-09-12 13:12:01 +0100

    commit

--- lib/screens/launch_screen.dart
+++ lib/screens/launch_screen.dart
@@ -1,1 +1,13 @@
+import 'package:copay/static/colors.dart';
 import 'package:flutter/material.dart';
+
+class LaunchScreen extends StatelessWidget {
+  const LaunchScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: primaryColor,
+    );
+  }
+}
