68fc954c90867f6f8b6f46025657556e843fe1c9
Sohmteee <sohmtee@gmail.com>
2022-09-12 13:07:54 +0100

    commit

--- lib/main.dart
+++ lib/main.dart
@@ -13,7 +13,7 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Co.Pay',
       theme: ThemeData(
-        primarySwatch: MaterialColor(primaryColor, swatch),
+        primarySwatch: primaryColorSwatch,
       ),
       home: const MyHomePage(title: 'Flutter Demo Home Page'),
     );
--- lib/static/colors.dart
+++ lib/static/colors.dart
@@ -2,18 +2,18 @@ import 'package:flutter/material.dart';
 
 Color primaryColor = Color.fromARGB(255, 29, 171, 136);
 
-const MaterialColor primaryColor = MaterialColor(
-  0xFFFFFFFF,
+const MaterialColor primaryColorSwatch = MaterialColor(
+  0x001DAB87,
   <int, Color>{
-    50: Color(0xFFFFFFFF),
-    100: Color(0xFFFFFFFF),
-    200: Color(0xFFFFFFFF),
-    300: Color(0xFFFFFFFF),
-    400: Color(0xFFFFFFFF),
-    500: Color(0xFFFFFFFF),
-    600: Color(0xFFFFFFFF),
-    700: Color(0xFFFFFFFF),
-    800: Color(0xFFFFFFFF),
-    900: Color(0xFFFFFFFF),
+    50: Color(0x001DAB87),
+    100: Color(0x001DAB87),
+    200: Color(0x001DAB87),
+    300: Color(0x001DAB87),
+    400: Color(0x001DAB87),
+    500: Color(0x001DAB87),
+    600: Color(0x001DAB87),
+    700: Color(0x001DAB87),
+    800: Color(0x001DAB87),
+    900: Color(0x001DAB87),
   },
 );
