ceda9cd57496dca37478729a61c1b28a1837fceb
Sohmteee <sohmtee@gmail.com>
2022-09-12 13:01:41 +0100

    commit

--- lib/main.dart
+++ lib/main.dart
@@ -13,7 +13,7 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Co.Pay',
       theme: ThemeData(
-        primarySwatch: MaterialColor(),
+        primarySwatch: ,
       ),
       home: const MyHomePage(title: 'Flutter Demo Home Page'),
     );
