7342c08650e9ecfed2d8232a15a72c3920da3eab
Sohmteee <sohmtee@gmail.com>
2022-09-15 09:19:42 +0100

    commit

--- lib/static/onboarding_template.dart
+++ lib/static/onboarding_template.dart
@@ -3,12 +3,12 @@ import 'package:flutter/material.dart';
 class OnBoarding extends StatelessWidget {
   const OnBoarding({Key? key}) : super(key: key);
 
+  final String mainPic, ;
+
   @override
   Widget build(BuildContext context) {
     return Column(
-      children: [
-        
-      ],
+      children: [],
     );
   }
 }
