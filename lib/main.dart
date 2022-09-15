7e09b4f4db86e5df6f0fa5f06b247b9a33ce48c3
Sohmteee <sohmtee@gmail.com>
2022-09-15 09:00:53 +0100

    commit

--- lib/main.dart
+++ lib/main.dart
@@ -13,7 +13,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return ScreenUtilInit(
-      designSize: ,
+      designSize: const Size(375,812),
       child: MaterialApp(
         title: 'Co.Pay',
         theme: ThemeData(
