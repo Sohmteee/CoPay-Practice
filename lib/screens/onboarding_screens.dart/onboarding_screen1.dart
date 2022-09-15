b5a53fa1d027ca23e203511ff89e58a199bbf075
Sohmteee <sohmtee@gmail.com>
2022-09-15 09:22:54 +0100

    commit

--- lib/screens/onboarding_screens.dart/onboarding_screen1.dart
+++ lib/screens/onboarding_screens.dart/onboarding_screen1.dart
@@ -1,1 +1,10 @@
-import 'fl';
+import 'package:flutter/material.dart';
+
+class OnBoarding1 extends StatelessWidget {
+  const OnBoarding1({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Column();
+  }
+}
